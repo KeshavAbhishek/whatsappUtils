fe68f4d77dea9111fcea3f264854105181fb0edd
Keshav-Abhishek <73807910+KeshavAbhishek@users.noreply.github.com>
2021-10-12 18:15:16 +0530

    WhatsApp Utils - sources

---
+++ whatsapp.js
@@ -0,0 +1,30 @@
+var stopper = 100;
+var x = 0;
+
+setInterval(()=>{
+	if(x<=stopper){
+		document.getElementById('whatsappBackground').style.filter = `invert(${x}%)`;
+		x+=1;
+	}
+
+	else {
+		document.getElementById('whatsappBackground').style.filter = 'invert(0%)';
+		x=0;
+	};
+
+
+}, 20);
+
+document.getElementById('chat').disabled = 'true';
+
+document.getElementById('generate').addEventListener('click',()=>{
+	var operableNumber = document.getElementById('numberEntry').value;
+
+	var removeables = ['(',')','-','+']
+
+	for (var i = 0; i < removeables.length; i++){
+		operableNumber = operableNumber.replace(removeables[i],'');
+	}
+
+	document.getElementById('chat').setAttribute('href',`https://wa.me/+${operableNumber}`);
+});
